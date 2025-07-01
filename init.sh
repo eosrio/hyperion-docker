76c60fb5af011dd52bfa4f949568012555c526fc
Bruno Leal <lealbrunocalhau@gmail.com>
2025-07-01 14:36:42 -0300

    Remove deprecated initialization and logging scripts for Docker setup

--- init.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Create a new docker network called hyperion
-docker network create hyperion
--- test.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-curl --cacert ca.crt -u elastic:changeme123 https://localhost:9200
